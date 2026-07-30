630667f1dd776f9ec352a656ee3aa3cec59c5e3e
Diganth <diganthhm714@gmail.com>
2026-07-30 16:19:17 +0530

    Sum of 2 numbers

--- basic math/sum.go
+++ basic math/sum.go
@@ -1,6 +1,9 @@
 package main
 import "fmt"
-func main()
-{
-	var num int 
+func main(){
+	var num int =10
+	var num1 int= 20
+	var sum int =0
+	sum=num+num1
+	fmt.Println("The sum of two numbers is = ",sum)
 }
