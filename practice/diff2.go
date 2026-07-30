168f1b30cfad42960b9e22b6eccae29594250fd4
Diganth <diganthhm714@gmail.com>
2026-07-30 18:54:20 +0530

    use defer

--- practice/diff2.go
+++ practice/diff2.go
@@ -3,9 +3,10 @@ package main
 import "fmt"
 func main(){
 	fmt.Println("Counting from 30 to 1 begins...")
+	 defer fmt.Println("your time is up...")
 	for i:=0 ;i<30;i++{
 		defer fmt.Println(i)
 	}
 
- defer fmt.Println("your time is up...")
+
 }
