e0f6e05d0869004b4003fa97f884f6dc1ec10f65
Diganth <diganthhm714@gmail.com>
2026-07-31 12:47:15 +0530

    use of packages

---
+++ practice/pac.go
@@ -0,0 +1,11 @@
+package main
+import  (
+	     "fmt"
+		 "math"
+         "math/rand"
+)
+
+func main(){
+	fmt.Println("Choosing a random number ",rand.Intn(100))
+	fmt.Println("The value of PI is = ",math.Pi)
+}
