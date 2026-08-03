497f32e54ccad22b1d62276d04903ca9e140bd40
Diganth <diganthhm714@gmail.com>
2026-08-03 22:39:55 +0530

    func in go

--- practice/func.go
+++ practice/func.go
@@ -4,11 +4,16 @@ import "fmt"
 func add(x,y int) int{
 	return x+y
 }
+//the above func can also be written as 
+var multi =func(x,y int)int {
+     return x*y
+}
 
 func main(){
 	var x,y int
 	fmt.Println("Enter the values for addition of X and y ")
 	fmt.Scan(&x,&y)
 	fmt.Println("The addtion of x and y is = ",add(x,y))
+	fmt.Println("The multiplication of x and y is = ",multi(x,y))
 
 }
