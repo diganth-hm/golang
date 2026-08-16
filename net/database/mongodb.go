c23dd7974bca62c5f1d813303cf93b27b5010ad6
Diganth <diganthhm714@gmail.com>
2026-08-16 22:00:11 +0530

    made database connection

---
+++ net/database/mongodb.go
@@ -0,0 +1,27 @@
+package database
+
+import (
+	"fmt"
+	"context"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
+)
+
+const connectionString ="mongodb://localhost:270177"
+
+var collectiiion *mongo.Collection
+
+func init(){
+	//client option
+    clientoption := options.Client().ApplyURI(connectionString)
+	//connect to mongodb
+	client,err := mongo.Connect(context.TODO(),clientoption)
+	if err != nil{
+		panic(err)
+	}
+	fmt.Println("Mongodb connection sucess")
+
+	collection=client.Database(dbName).Collection(colName)
+	//collection istance 
+	fmt.Println("collection istance is ready")
+}
