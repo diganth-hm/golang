709850e7378a3cfc4f534a57f4dfa1e3bba9abaa
Diganth <diganthhm714@gmail.com>
2026-08-21 22:10:46 +0530

    written controller to get all movies

--- net/controller/controller.go
+++ net/controller/controller.go
@@ -1,3 +1,16 @@
 package controller
 
-import ()
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/diganth-hm/golang/buildapi/database"
+)
+
+func GetAllmovies(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Println("Getting all movies")
+	allmovies := database.Getallmovies()
+	json.NewEncoder(w).Encode(allmovies)
+}
