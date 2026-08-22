a018c1396dbc1482805d633c3aba44b702645c79
Diganth <diganthhm714@gmail.com>
2026-08-22 20:45:49 +0530

    chose a port for the server and roted it

--- net/main.go
+++ net/main.go
@@ -2,8 +2,18 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/diganth-hm/golang/buildapi/routes"
 )
 
 func main() {
-	fmt.Println()
+	fmt.Println("MongoDB API")
+	fmt.Println("server is runnig .....")
+	fmt.Println("Server runing on http://localhost:5454")
+	//assing the route
+	r := routes.Router()
+	log.Fatal(http.ListenAndServe(":5454", r))
+
 }
