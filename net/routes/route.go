7a26b624d9030fc0cdc69692b30faf5a7ca7178c
Diganth <diganthhm714@gmail.com>
2026-08-22 19:59:49 +0530

    written routes

---
+++ net/routes/route.go
@@ -0,0 +1,16 @@
+package routes
+
+import (
+	"github.com/diganth-hm/golang/buildapi/controller"
+	"github.com/gorilla/mux"
+)
+
+func Router() *mux.Router {
+	router := mux.NewRouter()
+	router.HandleFunc("/movies", controller.GetAllmovies).Methods("GET")
+	router.HandleFunc("/movies/add", controller.Creatmovie).Methods("POST")
+	router.HandleFunc("/movies/delone", controller.DeleteAmovie).Methods("DELETE")
+	router.HandleFunc("/movies/delAll", controller.DeleteAll).Methods("DELETE")
+	router.HandleFunc("/movie/marksaswatched/{}", controller.MarkAswatched).Methods("PUT")
+	return router
+}
